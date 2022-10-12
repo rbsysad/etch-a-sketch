2f95de226c95b025879906b9e7f98f737e29d89a
rbsysad <rbsysad@gmail.com>
2022-10-12 11:41:52 -0500

    Add grid items logic and CSS

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
+const container = document.querySelector('#container');
 
+for (i = 0; i < 256; i++) {
+    const div = document.createElement('div');
+    container.appendChild(div).className = 'grid-item';
+}
